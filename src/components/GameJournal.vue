933afb48d8b1d05c323e773157c4bd7d5b2cdb26
springfil <st.kenzhaev@gmail.com>
2023-08-04 23:55:25 +0300

    splitting the battle log into components

--- src/components/GameJournal.vue
+++ src/components/GameJournal.vue
@@ -3,25 +3,12 @@
         <button @click="toggleModal()">
             {{ modalVisible ? 'Закрыть' : 'Открыть модальное окно' }}
         </button>
-        <div v-if="modalVisible" id="modal">
-            <div v-if="modalVisible" id="modal">
-                <h2>Боевой журнал</h2>
-                <transition-group name="slide-fade" tag="div">
-                    <ul>
-                        <li
-                            v-for="entry in log"
-                            :key="entry.time"
-                            class="log-entry">
-                            {{ entry.attacker }} нанес {{ entry.damage }} урона в {{ entry.time }}
-                        </li>
-                    </ul>
-                </transition-group>
-            </div>
-        </div>
+        <Modal v-if="modalVisible" :log="log" />
     </div>
 </template>
 
 <script setup>
+import Modal from './Modal.vue'
 import { ref, inject, watch } from 'vue'
 
 const damageMultiplier = inject('data')
@@ -38,32 +25,5 @@ function attack() {
     log.value.push({ attacker: 'Nezuko', damage, time })
 }
 
-watch(damageMultiplier, (newDamageMultiplier) => {
-    attack()
-})
+watch(damageMultiplier, attack)
 </script>
-
-<style scoped>
-.log-entry {
-    padding: 5px;
-    margin-bottom: 5px;
-    background-color: #9e2626;
-    border-radius: 5px;
-    width: 250px;
-    align-items: center;
-}
-
-.slide-fade-enter-active {
-    transition: all 0.3s ease-out;
-}
-
-.slide-fade-leave-active {
-    transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
-}
-
-.slide-fade-enter-from,
-.slide-fade-leave-to {
-    transform: translateX(20px);
-    opacity: 0;
-}
-</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,44 @@
+<template>
+    <div id="modal">
+        <h2>Боевой журнал</h2>
+        <transition-group name="slide-fade" tag="div">
+            <ul>
+                <li v-for="entry in log" :key="entry.time" class="log-entry">
+                    {{ entry.attacker }} нанес {{ entry.damage }} урона в
+                    {{ entry.time }}
+                </li>
+            </ul>
+        </transition-group>
+    </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps(['log'])
+</script>
+
+<style scoped>
+.log-entry {
+    padding: 5px;
+    margin-bottom: 5px;
+    background-color: #9e2626;
+    border-radius: 5px;
+    width: 250px;
+    align-items: center;
+}
+
+.slide-fade-enter-active {
+    transition: all 0.3s ease-out;
+}
+
+.slide-fade-leave-active {
+    transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+    transform: translateX(20px);
+    opacity: 0;
+}
+</style>
